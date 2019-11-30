5a86d9b63eb62667b234503cc985beb9939a4006
emredv <33000794+emredv@users.noreply.github.com>
2019-11-30 11:13:00 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 
-$config->displayError();
 
-$config->languageDetect();
+$config->displayError();   // Oluşan hataları gösterir
+$config->languageDetect(); // Dil dosyalarını yükler (/lang)
+$config->cronJobs(true);   // Zamanlanmış görevleri aktifleştirir
+$config->autoBackup();     // Otomatik yedeklemeyi aktifleştirir
 
-//$config->mysqlServer();
-//$config->mysqlDatabase();
-//$config->mysqlUsername();
-//$config->mysqlPassword();
-//$config->mysqlConnection();
+//$config->mysqlDatabase(''); // MySql veritabanı adı
+//$config->mysqlUsername(''); // MySql kullanıcı adı
+//$config->mysqlPassword(''); // MySql Parolası
+//$config->mysqlConnection(); // Otomatik Mysql bağlantısı
