441ca925e968fdf337eb08f09ea505a939b3d184
emredv <33000794+emredv@users.noreply.github.com>
2020-01-21 17:26:50 +0300

    Update candy.js

--- assets/js/candy.js
+++ assets/js/candy.js
@@ -1,16 +1,30 @@
-class Candy {
-test(){
-  alert('Hi, World');
-}
-showModal(s){
-  $('#' + s).modal('show');
-}
-get(url,callback){
-  var data;
-  var status;
-  $.get(url, function(data, status){
-      callback(data,status);
-  });
-}
+var _token = '';
+var Candy = class Candy {
+  test(){
+    alert('Hi, World');
+  }
+  showModal(s){
+    $('#' + s).modal('show');
+  }
+  get(url,callback){
+    var data;
+    var status;
+    $.get(url, function(data, status){
+      callback(data,status)
+    });
+  }
+  getToken(){
+    $.get(window.location.hostname+'?_candy=token',function(data){
+      var result = JSON.parse(JSON.stringify(data));
+      _token = result.token;
+    });
+  }
+  token(){
+    candy.getToken();
+    return _token;
+  }
 }
 var candy = new Candy;
+$(function(){
+  candy.getToken();
+});
