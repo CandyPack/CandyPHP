11635a7fb5d65c1fb11667204b133ae1ad717f53
emredv <33000794+emredv@users.noreply.github.com>
2020-02-29 19:54:01 +0300

    Add files via upload

--- sys/sys_settings.php
+++ sys/sys_settings.php
@@ -214,6 +214,17 @@ class Config {
     }
   }
 
+  public static function composer($b=true){
+    if(!defined('CANDY_COMPOSER')){
+      if(is_bool($b)){
+        define('CANDY_COMPOSER',$b);
+      }else{
+        define('CANDY_COMPOSER', true);
+        define('CANDY_COMPOSER_DIRECTORY', $b);
+      }
+    }
+  }
+  
   public static function check($v){
     $return = true;
     $arr_var = explode(',',$v);
