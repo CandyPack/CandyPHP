b9cde26fd286619ece706df1491a642d48ec1bf4
emredv <33000794+emredv@users.noreply.github.com>
2020-06-03 14:48:20 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,13 @@
 <?php
 
-$config->displayError();   // Oluşan hataları gösterir
-$config->languageDetect(); // Dil dosyalarını yükler (/lang)
-$config->cronJobs(true);   // Zamanlanmış görevleri aktifleştirir
-$config->autoBackup();     // Otomatik yedeklemeyi aktifleştirir
-$config->composer(false);  // True veya autoload.php dizini girilebilir.
+Config::displayError();   // Oluşan hataları gösterir
+Config::languageDetect(); // Dil dosyalarını yükler (/lang)
+Config::cronJobs();       // Zamanlanmış görevleri aktifleştirir
+Config::composer();       // True veya autoload.php dizini girilebilir.
+Config::masterMail(/*E-Mail Adresi*/) // Oluşan önemli sorunları mail ile raporlar.
+  
+//Config::mysql(/*tanımlanan isim(opsiyonel)*/)->database(/*Veritabanı*/)->user(/*Kullanıcı*/)->password(/*Parola*/); // Veritabanı tanımlar
+//Config::mysqlConnection(); // Otomatik Mysql bağlantısı
 
-//$config->mysqlDatabase(''); // MySql veritabanı adı
-//$config->mysqlUsername(''); // MySql kullanıcı adı
-//$config->mysqlPassword(''); // MySql Parolası
-//$config->mysqlConnection(); // Otomatik Mysql bağlantısı
-
-$config->autoBackup();  // Günlük yedek alır
-$config->autoUpdate();  // Candy PHP yeni sürümü çıktığında günceller
+Config::autoBackup();  // Günlük yedek alır
+Config::autoUpdate();  // Candy PHP yeni sürümü çıktığında günceller
