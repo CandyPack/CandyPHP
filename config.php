263e0640c0c5b036740f6b24c7474a7d3be6a98d
emredv <33000794+emredv@users.noreply.github.com>
2020-06-23 21:10:26 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ Config::displayError();   // Oluşan hataları gösterir
 Config::languageDetect(); // Dil dosyalarını yükler (/lang)
 Config::cronJobs();       // Zamanlanmış görevleri aktifleştirir
 Config::composer();       // True veya autoload.php dizini girilebilir.
-Config::masterMail(/*E-Mail Adresi*/) // Oluşan önemli sorunları mail ile raporlar.
+Config::masterMail(/*E-Mail Adresi*/); // Oluşan önemli sorunları mail ile raporlar.
   
 //Config::mysql(/*tanımlanan isim(opsiyonel)*/)->database(/*Veritabanı*/)->user(/*Kullanıcı*/)->password(/*Parola*/); // Veritabanı tanımlar
 //Config::mysqlConnection(); // Otomatik Mysql bağlantısı
