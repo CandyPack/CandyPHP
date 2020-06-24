533f672e480af8a1e6488ed20e90deb95a77d63e
emredv <33000794+emredv@users.noreply.github.com>
2020-06-24 10:16:54 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,7 @@ Config::cronJobs();       // Zamanlanmış görevleri aktifleştirir
 Config::composer();       // True veya autoload.php dizini girilebilir.
 Config::masterMail(/*E-Mail Adresi*/); // Oluşan önemli sorunları mail ile raporlar.
   
-//Config::mysql(/*tanımlanan isim(opsiyonel)*/)->database(/*Veritabanı*/)->user(/*Kullanıcı*/)->password(/*Parola*/); // Veritabanı tanımlar
-//Config::mysqlConnection(); // Otomatik Mysql bağlantısı
+//Config::mysql(/*tanımlanan isim(opsiyonel)*/)->database(/*Veritabanı*/)->user(/*Kullanıcı*/)->password(/*Parola*/)->default(); // Veritabanı tanımlar
 
 Config::autoBackup();  // Günlük yedek alır
 Config::autoUpdate();  // Candy PHP yeni sürümü çıktığında günceller
