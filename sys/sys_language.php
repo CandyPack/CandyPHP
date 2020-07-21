2cdd8b0985c8deaa8079363d0c89bec254f3c04d
emredv <33000794+emredv@users.noreply.github.com>
2020-07-21 07:43:19 +0000

    Add files via upload

--- sys/sys_language.php
+++ sys/sys_language.php
@@ -2,15 +2,51 @@
 class Lang {
   public $lang;
 
-  public static function get($var){
-    return $GLOBALS['_lang'][$var];
+  public static function get($var, $arr = null){
+    $return =isset($GLOBALS['_lang'][$var]) ? $GLOBALS['_lang'][$var] : "";
+    if($array!=null){
+      if(is_array($arr)){
+        $loop = 0;
+        foreach ($arr as $key) {
+          $return = str_replace('$'.$loop, $key, $return);
+          $loop++;
+        }
+      }else{
+        $return = str_replace('$0', $arr, $return);
+      }
+    }
+    return $return;
   }
 
   public static function echo($var){
-    echo $GLOBALS['_lang'][$var];
+    $return = isset($GLOBALS['_lang'][$var]) ? $GLOBALS['_lang'][$var] : "";
+    if($array!=null){
+      if(is_array($arr)){
+        $loop = 0;
+        foreach ($arr as $key) {
+          $return = str_replace('$'.$loop, $key, $return);
+          $loop++;
+        }
+      }else{
+        $return = str_replace('$0', $arr, $return);
+      }
+    }
+    echo $return;
   }
 
   public static function setArray($arr){
     $GLOBALS['_lang'] = $arr;
   }
+
+  public static function set($code){
+    function returnLang($l){
+      return require_once "lang/{$l}.php";
+    }
+    if(file_exists("lang/{$code}.php")){
+      Lang::setArray(returnLang($code));
+    }elseif(file_exists("lang/lang.php")){
+      Lang::setArray(returnLang('lang'));
+    }
+  }
+
 }
