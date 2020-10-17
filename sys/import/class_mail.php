9504bc22d8a3fe05173870026f605eb3f2a419d9
emredv <33000794+emredv@users.noreply.github.com>
2020-10-17 12:54:05 +0300

    Add files via upload

--- sys/import/class_mail.php
+++ sys/import/class_mail.php
@@ -2,18 +2,22 @@
 class Mail
 {
     private static $_arr = [];
-
+
+    function __construct($_arr=[]){
+      self::$_arr = $_arr;
+    }
+
     public static function view($v){
       self::$_arr['view'] = $v;
-      return new static();
+      return new static(self::$_arr);
     }
     public static function to($v){
       self::$_arr['to'] = $v;
-      return new static();
+      return new static(self::$_arr);
     }
     public static function subject($v){
       self::$_arr['subject'] = $v;
-      return new static();
+      return new static(self::$_arr);
     }
     public static function from($email,$name = ''){
       if($name!=''){
@@ -21,7 +25,7 @@ class Mail
       }else{
         self::$_arr['from'] = $email;
       }
-      return new static();
+      return new static(self::$_arr);
     }
     public static function send(){
       if(isset(self::$_arr['to']) && isset(self::$_arr['view'])){
@@ -30,6 +34,34 @@ class Mail
             return Candy::get($v);
           }
         }
+        if(!file_exists('view/mail/'.self::$_arr['view'].'.blade.php')){
+          $storage = Candy::storage('sys')->get('mail');
+          $storage->error = isset($storage->error) && is_object($storage->error) ? $storage->error : new \stdClass;
+          $storage->error->info = isset($storage->error->info) && is_object($storage->error->info) ? $storage->error->info : new \stdClass;
+          if(Config::check('MASTER_MAIL') && (!isset($storage->error->info->date) || $storage->error->info->date!=date('d/m/Y'))){
+            Candy::quickMail(MASTER_MAIL,
+            '<b>Date</b>: '.date("Y-m-d H:i:s").'<br />
+            <b>Message</b>: Couldn\'t send mail <br />mail/'.self::$_arr['view'].'.blade.php <b>Not Exists File</b><br /><br />
+            MAIL:
+            <pre>'.print_r(self::$_arr,true).'</pre><br /><br />
+            <b>Details</b>: <br />
+            SERVER:
+            <pre>'.print_r($_SERVER,true).'</pre>
+            SESSION:
+            <pre>'.print_r($_SESSION,true).'</pre>
+            COOKIE:
+            <pre>'.print_r($_COOKIE,true).'</pre>
+            POST:
+            <pre>'.print_r($_POST,true).'</pre>
+            GET:
+            <pre>'.print_r($_GET,true).'</pre>',
+            $_SERVER['SERVER_NAME'].' - INFO',
+            ['mail' => 'candyphp@'.$_SERVER['SERVER_NAME'], 'name' => 'Candy PHP']);
+          $storage->error->info->date = date('d/m/Y');
+          Candy::storage('sys')->set('mail',$storage);
+        }
+          return false;
+        }
         ob_start();
         include(View::cacheView('mail/'.self::$_arr['view'].'.blade.php'));
         $message = ob_get_clean();
