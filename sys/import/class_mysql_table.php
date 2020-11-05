49a7448a55dad18ebc4ee61489382df3dd020a79
emredv <33000794+emredv@users.noreply.github.com>
2020-11-05 12:41:49 +0300

    Add files via upload

--- sys/import/class_mysql_table.php
+++ sys/import/class_mysql_table.php
@@ -79,14 +79,10 @@ class Mysql_Table {
   function set($arr,$val=null){
     $vars = "";
     if(!is_array($arr) && $val !== null){
-      $k = isset($arr['v']) ? " " . $arr['v'] . " " : "`".Mysql::escape($arr)."`";
-      $v = is_numeric($val) ? $val : (isset($val['v']) && isset($val['ct']) && $val['ct']==$GLOBALS['candy_token_mysql'] ? " " . $val['v'] . " " : '"'.Mysql::escape($val).'"');
-      $vars .= $k.' = '. $v .',';
+      $vars .= self::escape($arr,'col').' = '. self::escape($val) .',';
     }else{
       foreach($arr as $key => $val) {
-        $k = isset($key['v']) ? " " . $key['v'] . " " : "`".Mysql::escape($key)."`";
-        $v = is_numeric($val) ? $val : (isset($val['v']) && isset($val['ct']) && $val['ct']==$GLOBALS['candy_token_mysql'] ? " " . $val['v'] . " " : '"'.Mysql::escape($val).'"');
-        $vars .= $k.' = '. $v .',';
+        $vars .= self::escape($key,'col').' = '. self::escape($val) .',';
       }
     }
     $query = "UPDATE `".$this->arr['table']."` SET ".substr($vars,0,-1)." ".self::query();
@@ -104,8 +100,8 @@ class Mysql_Table {
     $query_key = '';
     $query_val = '';
     foreach ($arr as $key => $val){
-      $query_key .= '`'.Mysql::escape($key).'`,';
-      $query_val .= is_numeric($val) ? $val.',' : (isset($val['v']) && isset($val['ct']) && $val['ct']==$GLOBALS['candy_token_mysql'] ? $val['v'].',' : '"'.Mysql::escape($val).'",');
+      $query_key .= self::escape($key,'col');
+      $query_val .= self::escape($val);
     }
     $query = "INSERT INTO `".$this->arr['table']."` ".' ('.substr($query_key,0,-1).') VALUES ('.substr($query_val,0,-1).')';
     $sql = mysqli_query(Mysql::$conn, $query);
@@ -128,23 +124,21 @@ class Mysql_Table {
   function select(){
     $this->arr['select'] = isset($this->arr['select']) ? $this->arr['select'] : '';
     $select = array_filter(explode(',',$this->arr['select']));
-    foreach(func_get_args() as $key){
-      if(is_array($key) && (!isset($key['v']) || !isset($key['ct']) || $key['ct']!=$GLOBALS['candy_token_mysql'])){
-      }else{
-        if(isset($key['v']) && isset($key['ct']) && $key['ct']==$GLOBALS['candy_token_mysql']){
-          $select[] = $key['v'];
-        }else{
-          $exp_key = explode('.',$key);
-          $select[] = isset($exp_key[1]) ? Mysql::escape($exp_key[0]).".`".Mysql::escape($exp_key[1])."`" : "`".Mysql::escape($key)."`";
-        }
+    if(count(func_get_args())==1 && is_array(func_get_args()[0])){
+      foreach(func_get_args()[0] as $key => $value){
+        $select[] = self::escape($key,'col').' AS '.self::escape($value);
+        $this->arr['select'] = implode(', ',$select);
       }
-      $this->arr['select'] = implode(', ',$select);
+    }else{
+      foreach(func_get_args() as $key){
+        $select[] = self::escape($key,'col');
+        $this->arr['select'] = implode(', ',$select);
+      }
     }
     return new static($this->arr);
   }
   function orderBy($v1,$v2='asc'){
-    $v1 = is_array($v1) && isset($v1['v']) ? $v1['v'] : "`".Mysql::escape($v1)."`";
-    $this->arr['order by'] = $v1.' '.($v2 == 'desc' || $v2 == 'DESC' ? 'DESC' : 'ASC');
+    $this->arr['order by'] = self::escape($v1,'col').(strtolower($v2) == 'desc' ? 'DESC' : 'ASC');
     return new static($this->arr);
   }
   function limit($v1,$v2=null){
@@ -159,20 +153,11 @@ class Mysql_Table {
   }
   function join($tb,$col1,$st,$col2=null,$type='inner join'){
     $this->arr[$type] = isset($this->arr[$type]) ? $this->arr[$type] : [];
-    $tb = "`".Mysql::escape($tb)."`";
-    $exp_col1 = explode('.',$col1);
-    $col1 = isset($exp_col1[1]) ? Mysql::escape($exp_col1[0]).".`".Mysql::escape($exp_col1[1])."`" : "`".Mysql::escape($col1)."`";
-    if($col2 !== null){
-      $exp_col2 = explode('.',$col2);
-      $col2 = isset($exp_col2[1]) ? Mysql::escape($exp_col2[0]).".`".Mysql::escape($exp_col2[1])."`" : "`".Mysql::escape($col2)."`";
-      $state = in_array(strtoupper($st),$this->statements) ? strtoupper($st) : "=";
-    }else{
-      $exp_col2 = explode('.',$st);
-      $col2 = isset($exp_col2[1]) ? Mysql::escape($exp_col2[0]).".`".Mysql::escape($exp_col2[1])."`" : "`".Mysql::escape($st)."`";
-      $state = "=";
-    }
-
-    $this->arr[$type][] = $tb . ' ON ' . $col1 . ($state . $col2);
+    $tb = self::escape($tb,'col');
+    $col1 = self::escape($col1,'col');
+    $col2 = self::escape(($col2 !== null ? $col2 : $st),'col');
+    $state = self::escape(($col2 !== null ? $st : '='),'st');
+    $this->arr[$type][] = $tb . ' ON ' . $col1 . $state . $col2;
     return new static($this->arr);
   }
   private function whereExtract($arr){
@@ -190,7 +175,7 @@ class Mysql_Table {
         if(in_array(strtoupper($key),$this->val_statements)){
           $q .= " ".strtoupper($key);
         }else{
-          $q .= isset($key['v']) ? " = " . $key['v'] . " " : " = '" . Mysql::escape($key) . "' ";
+          $q .= " =" . self::escape($key);
         }
       }elseif($in_arr){
         $q .= strtoupper($key)=='OR' ? " OR " : " AND ";
@@ -200,24 +185,31 @@ class Mysql_Table {
         $q .= " ".$state;
         $last = 1;
       }else{
-        if((!isset($key['ct']) || $key['ct']!=$GLOBALS['candy_token_mysql']) && is_array($key) && ($state == 'IN' || $state == 'NOT IN')){
-          $esc = [];
-          foreach ($key as $val){
-            $esc[] = Mysql::escape($val);
-          }
-          $q .= " ('".implode("','",$esc)."')";
-        }else{
-          if($loop==1){
-            $q .= isset($key['v']) ? " " . $key['v'] . " " : " `".Mysql::escape($key)."`";
-          }else{
-            $q .= isset($key['v']) ? " " . $key['v'] . " " : ' "'.Mysql::escape($key).'"';
-          }
-        }
+        $q .= ' '.self::escape($key,($loop==1 ? 'table' : 'value')).' ';
         $last = 1;
       }
         $loop++;
     }
     return '('.$q.')';
+  }
+  private function escape($v,$type = 'value'){
+    if(is_array($v) && isset($v['ct']) && isset($v['v']) && $v['ct']==$GLOBALS['candy_token_mysql']) return ' '.$v['v'].' ';
+    if($type == 'value'){
+      if(is_numeric($v)) return $v;
+      if(is_array($v)) return ' ("'.implode('","',array_map(function($val){return(Mysql::escape($val));},$v)).'") ';
+      return ' "'.Mysql::escape($v).'" ';
+    }elseif($type == 'table' || $type == 'col'){
+      $as = "";
+      if(is_array($v)){
+        $as = array_values($v)[0];
+        $v = array_keys($v)[0];
+        $as = "AS $as ";
+      }
+      if(strpos($v,'.') !== false) return ' `'.implode('`.`',array_map(function($val){return(Mysql::escape($val));},explode('.',$v))).'` '.$as;
+      return ' `'.Mysql::escape($v).'` '.$as;
+    }elseif($type == 'statement' || $type == 'st'){
+      return in_array(strtoupper($v),$this->statements) ? strtoupper($v) : "=";
+    }
   }
   private function error($caller){
     if(Candy::isDev()) printf("Candy Mysql Error: %s\n<br />".$caller['file'].' : '.$caller['line'], mysqli_error(Mysql::$conn));
