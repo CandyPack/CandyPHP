e84b415f3528edf7666039e86434fbbca5e9cef0
emredv <33000794+emredv@users.noreply.github.com>
2020-11-17 18:44:16 +0300

    Add files via upload

--- sys/sys_settings.php
+++ sys/sys_settings.php
@@ -391,6 +391,7 @@ class Config {
   }
 
   public static function errorReport($type,$mssg=null,$file=null,$line=null){
+    return false;
     if(Candy::isDev()) return true;
     $now = date('YmdH');
     $storage = Candy::storage('sys')->get('error');
