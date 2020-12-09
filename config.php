40b42c61fa698f01dec125cd3115824292462657
emredv <33000794+emredv@users.noreply.github.com>
2020-12-09 13:15:57 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
-Config::languageDetect(/* Default Language (en) */); // Activates language files (/lang)
-Config::cronJobs();       // Activates scheduled tasks
-Config::composer();       // True or autoload.php directory can be entered.
-Config::key(/*Key*/);     // A key must be entered for the encryption method. It can only be decrypted with the same key.
+Config::language(/* Default Language (en) */); // Activates language files (/lang)
+Config::cron();       // Activates scheduled tasks
+Config::composer();   // True or autoload.php directory can be entered.
+Config::key(/*Key*/); // A key must be entered for the encryption method. It can only be decrypted with the same key.
   
 //Config::mysql(/*identified name(optional)*/)->database(/*Database*/)->user(/*User*/)->password(/*Password*/)->default(); // Define a database
 
-//Config::devmode(true) // If true, activates developer mode.
-//        ->errors()    // Displays errors that have occurred
-//        ->mail(/*E-Mail Address*/) // It reports important problems by mail.
-//        ->version('2020-01-30');   // Runs the version of the site on the date entered.
+//Config::dev(true) // If true, activates developer mode.
+//      ->errors()    // Displays errors that have occurred
+//      ->mail(/*E-Mail Address*/) // It reports important problems by mail.
+//      ->version('2020-01-30');   // Runs the version of the site on the date entered.
 
-Config::autoBackup();  // Takes daily backups
-Config::autoUpdate();  // Candy PHP updates when new version is released
+Config::backup();  // Takes daily backups
+Config::update();  // Candy PHP updates when new version is released
