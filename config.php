84602bf7d72e80d8d21840c5750da7e897563b8b
Emre YILMAZ <33000794+EmreRed@users.noreply.github.com>
2021-04-09 11:57:25 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,15 @@
 //Config::mysql(/*identified name(optional)*/)->database(/*Database*/)->user(/*User*/)->password(/*Password*/)->backup()->abort(/*Code*/)->default(); // Define a database
 
 //Config::dev(true) // If true, activates developer mode.
-//      ->errors()    // Displays errors that have occurred
+//      ->errors()  // Displays errors that have occurred
 //      ->mail(/*E-Mail Address*/) // It reports important problems by mail.
 //      ->version('2020-01-30');   // Runs the version of the site on the date entered.
+
+//Config::auth() // Auto user detection
+      //->db('mysql','name') // Database type and name (Optional)
+      //->table('tb_user') // User table name
+      //->key('id') // Primary key column in the user table (Optional)
+      //->token(); // Enables cookies (Optional)
 
 Config::language(/* Default Language (en) */); // Activates language files (/lang)
 Config::composer(); // True or autoload.php directory can be entered.
@@ -13,5 +19,4 @@ Config::backup();  // Takes daily backups
 Config::update(); // Candy PHP updates when new version is released
 Config::brute(); // Brute Force protection
 Config::cron(); // Activates scheduled tasks
-Config::auth(); // Auto user detection
 Config::key(/*Key*/); // A key must be entered for the encryption method. It can only be decrypted with the same key.
