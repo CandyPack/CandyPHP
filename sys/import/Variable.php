21abdb482e911bfad4cd09ac3473a5d5965d1e61
Emre YILMAZ <33000794+EmreRed@users.noreply.github.com>
2022-01-12 10:30:20 +0300

    Add files via upload

--- sys/import/Variable.php
+++ sys/import/Variable.php
@@ -65,6 +65,7 @@ class Variable{
     if(in_array('numeric',          $val)) $result = (($result || $any) && (($any && $result) || is_numeric($this->str) ));
     if(in_array('url',              $val)) $result = (($result || $any) && (($any && $result) || preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$this->str) ));
     if(in_array('emoji',            $val)) $result = (($result || $any) && (($any && $result) || preg_match('/([0-9#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u',$this->str) ));
+    if(in_array('xss',              $val)) $result = (($result || $any) && (($any && $result) || strip_tags($this->str) == $this->str ));
     return $result;
   }
 
